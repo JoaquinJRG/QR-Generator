4d901aa1891fc666b3dd35cf8156c97560818f58
Joaquin <joaquinjrg01@gmail.com>
2024-07-08 12:19:21 +0200

    add github link

--- client/src/components/MyHeader.jsx
+++ client/src/components/MyHeader.jsx
@@ -8,7 +8,7 @@ export function MyHeader() {
         <QrCode width={40} height={40} strokeWidth={1} />
         <h1 className="text-2xl font-bold">QR CODE GENERATOR</h1>
       </div >
-      <a href="" className="flex items-center gap-3 cursor-pointer hover:underline">
+      <a href="https://github.com/JoaquinJRG/QR-Generator" className="flex items-center gap-3 cursor-pointer hover:underline">
         <GitHub width={20} height={20} />
         <h3>GitHub</h3>
       </a>
